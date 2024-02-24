92fafaac0a5aab7d9b42c9664a91ca74a4f09843
coderstop244 <152056461+coderstop244@users.noreply.github.com>
2024-02-24 07:25:10 -0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,34 @@
+#function for adding two numbers
+def add(x,y):
+    return x+y
+#funtion for subtracting two numbers
+def subtract(x,y):
+    return x-y
+#function for divinding two numbers
+def divide(x,y):
+    return x/y
+#function for multiprication of two numbers
+def multiply(x,y):
+    return x*y
+
+print("select operators")
+print("1.add")
+print("2.subtract")
+print("3.divdide")
+print("4.multiply")
+    #Take input from the user
+    
+num1=float(input("Enter the first number:-"))
+num2=float(input("Enter the second number:-"))
+choice=int(input("Enterchoice:"))
+
+if(choice==1):
+    print("Addition:-",add(num1,num2))
+elif(choice==2):
+    print("Addition:-",subtract(num1,num2))
+elif(choice==3):
+    print("Addition:-",divide(num1,num2))
+elif(choice==4):
+    print("Addition:-",multiply(num1,num2))
+else:
+    print("Invalid ")
---
+++ main.py
@@ -0,0 +1,9 @@
+
+print("Hello world",9,5, sep='-',end='0009\n')
+print("Mihir")
+a =1
+b="MIHIR"
+print(a)
+print(b)
+print("Type of a is:-",type(a))
+print("Type of a is:-",type(b))
