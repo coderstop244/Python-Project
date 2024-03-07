0803a942197eb9b64d04bfab7fe589cd30062194
coderstop244 <152056461+coderstop244@users.noreply.github.com>
2024-03-07 23:38:48 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+user_input=input("Hello this is Jarvis\nHow can I help you:-")
+if user_input=="Hello":
+  print("Hello this is jarvis how can I help you")
+elif user_input=="What is your name?":
+  print("My name is Jarvis")
+elif user_input=="What is your age?":
+  print("I am a computer program so I don't have an age")
+elif user_input=="What is your favaourite color?":
+  print("I don't have a favourite color")
+elif user_input=="What is your favaourite food?":
+  print("I don't have a favourite food")
+elif user_input=="Can you solve a math problem for me?":
+  print("sure,what is the problem?")
+elif user_input=="History of your existence? ":
+   print("I was created by team of elite society in 2024")
+elif user_input=="Who is owner of elite society?":
+  print("Mihir Dutta is the owner of the elite society")
+elif user_input=="Who is Mihir Dutta?":
+  print("Mihir Dutta is the owner of the elite society")
+elif user_input=="What is your purpose?":
+  print("My purpose is to help you")
+else:
+  print("I don't understand")
